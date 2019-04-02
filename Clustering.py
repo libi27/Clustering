10e0f4b7b96946d4d5d2b3ba959c12ecc3f8a083
Lirane Bitton <lirane@cs.huji.ac.il>
2019-04-02 11:50:53 +0300

    first commit

---
+++ Clustering.py
@@ -0,0 +1,186 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+
+def circles_example():
+    """
+    an example function for generating and plotting synthetic data.
+    """
+
+    t = np.arange(0, 2 * np.pi, 0.03)
+    length = np.shape(t)
+    length = length[0]
+    circle1 = np.matrix([np.cos(t) + 0.1 * np.random.randn(length),
+                         np.sin(t) + 0.1 * np.random.randn(length)])
+    circle2 = np.matrix([2 * np.cos(t) + 0.1 * np.random.randn(length),
+                         2 * np.sin(t) + 0.1 * np.random.randn(length)])
+    circle3 = np.matrix([3 * np.cos(t) + 0.1 * np.random.randn(length),
+                         3 * np.sin(t) + 0.1 * np.random.randn(length)])
+    circle4 = np.matrix([4 * np.cos(t) + 0.1 * np.random.randn(length),
+                         4 * np.sin(t) + 0.1 * np.random.randn(length)])
+    circles = np.concatenate((circle1, circle2, circle3, circle4), axis=1)
+
+    plt.plot(circles[0,:], circles[1,:], '.k')
+    plt.show()
+
+
+def apml_pic_example(path='APML_pic.pickle'):
+    """
+    an example function for loading and plotting the APML_pic data.
+
+    :param path: the path to the APML_pic.pickle file
+    """
+
+    with open(path, 'rb') as f:
+        apml = pickle.load(f)
+
+    plt.plot(apml[:, 0], apml[:, 1], '.')
+    plt.show()
+
+
+def microarray_exploration(data_path='microarray_data.pickle',
+                            genes_path='microarray_genes.pickle',
+                            conds_path='microarray_conds.pickle'):
+    """
+    an example function for loading and plotting the microarray data.
+    Specific explanations of the data set are written in comments inside the
+    function.
+
+    :param data_path: path to the data file.
+    :param genes_path: path to the genes file.
+    :param conds_path: path to the conds file.
+    """
+
+    # loading the data:
+    with open(data_path, 'rb') as f:
+        data = pickle.load(f)
+    with open(genes_path, 'rb') as f:
+        genes = pickle.load(f)
+    with open(conds_path, 'rb') as f:
+        conds = pickle.load(f)
+
+    # look at a single value of the data matrix:
+    i = 128
+    j = 63
+    print("gene: " + str(genes[i]) + ", cond: " + str(
+        conds[0, j]) + ", value: " + str(data[i, j]))
+
+    # make a single bar plot:
+    plt.figure()
+    plt.bar(np.arange(len(data[i, :])) + 1, data[i, :])
+    plt.show()
+
+    # look at two conditions and their correlation:
+    plt.figure()
+    plt.scatter(data[:, 27], data[:, 29])
+    plt.plot([-5,5],[-5,5],'r')
+    plt.show()
+
+    # see correlations between conds:
+    correlation = np.corrcoef(np.transpose(data))
+    plt.figure()
+    plt.imshow(correlation, extent=[0, 1, 0, 1])
+    plt.colorbar()
+    plt.show()
+
+    # look at the entire data set:
+    plt.figure()
+    plt.imshow(data, extent=[0, 1, 0, 1], cmap="hot", vmin=-3, vmax=3)
+    plt.colorbar()
+    plt.show()
+
+
+def euclid(X, Y):
+    """
+    return the pair-wise euclidean distance between two data matrices.
+    :param X: NxD matrix.
+    :param Y: MxD matrix.
+    :return: NxM euclidean distance matrix.
+    """
+
+    # TODO: YOUR CODE HERE
+
+
+def euclidean_centroid(X):
+    """
+    return the center of mass of data points of X.
+    :param X: a sub-matrix of the NxD data matrix that defines a cluster.
+    :return: the centroid of the cluster.
+    """
+
+    # TODO: YOUR CODE HERE
+
+
+def kmeans_pp_init(X, k, metric):
+    """
+    The initialization function of kmeans++, returning k centroids.
+    :param X: The data matrix.
+    :param k: The number of clusters.
+    :param metric: a metric function like specified in the kmeans documentation.
+    :return: kxD matrix with rows containing the centroids.
+    """
+
+    # TODO: YOUR CODE HERE
+
+
+def kmeans(X, k, iterations=10, metric=euclid, center=euclidean_centroid, init=kmeans_pp_init):
+    """
+    The K-Means function, clustering the data X into k clusters.
+    :param X: A NxD data matrix.
+    :param k: The number of desired clusters.
+    :param iterations: The number of iterations.
+    :param metric: A function that accepts two data matrices and returns their
+            pair-wise distance. For a NxD and KxD matrices for instance, return
+            a NxK distance matrix.
+    :param center: A function that accepts a sub-matrix of X where the rows are
+            points in a cluster, and returns the cluster centroid.
+    :param init: A function that accepts a data matrix and k, and returns k initial centroids.
+    :param stat: A function for calculating the statistics we want to extract about
+                the result (for K selection, for example).
+    :return: a tuple of (clustering, centroids, statistics)
+    clustering - A N-dimensional vector with indices from 0 to k-1, defining the clusters.
+    centroids - The kxD centroid matrix.
+    """
+
+    # TODO: YOUR CODE HERE
+
+
+def gaussian_kernel(X, sigma):
+    """
+    calculate the gaussian kernel similarity of the given distance matrix.
+    :param X: A NxN distance matrix.
+    :param sigma: The width of the Gaussian in the heat kernel.
+    :return: NxN similarity matrix.
+    """
+
+    # TODO: YOUR CODE HERE
+
+
+def mnn(X, m):
+    """
+    calculate the m nearest neighbors similarity of the given distance matrix.
+    :param X: A NxN distance matrix.
+    :param m: The number of nearest neighbors.
+    :return: NxN similarity matrix.
+    """
+
+    # TODO: YOUR CODE HERE
+
+
+def spectral(X, k, similarity_param, similarity=gaussian_kernel):
+    """
+    Cluster the data into k clusters using the spectral clustering algorithm.
+    :param X: A NxD data matrix.
+    :param k: The number of desired clusters.
+    :param similarity_param: m for mnn, sigma for the Gaussian kernel.
+    :param similarity: The similarity transformation of the data.
+    :return: clustering, as in the kmeans implementation.
+    """
+
+    # TODO: YOUR CODE HERE
+
+
+if __name__ == '__main__':
+
+    # TODO: YOUR CODE HERE
